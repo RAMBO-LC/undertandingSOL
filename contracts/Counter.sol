45060bb8c17153f3401ce25d3969bad0a1b4147e
RAMBO-LC <rambo4code@gmail.com>
2026-05-13 00:13:13 +0530

    FIRST CONTRACT

--- contracts/Counter.sol
+++ contracts/Counter.sol
@@ -1,11 +1,20 @@
+// SPDX-License-Identifier: MIT
+
 pragma solidity ^0.8.0;
 
 contract Counter {
     uint count;
     //1, 2, 3, .... but cant be -ve.
 
-    function getCount() {
+    constructor() public {
+        count = 0;
+    }
+
+    function getCount() public view returns(uint) {
         return count;
     }
 
+    function incrementCount() public {
+        count += 1;
+    }
 }
